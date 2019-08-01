e9b7e2d4d1e5874c0c17288b8bcbb97993f39103
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2019-08-01 15:08:48 -0400

    Finished fixing design

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
-(() => {
-//this is a self invoking function
-
-console.log('my JS is workin!!!');
-})();
-
 // VARIABLES
-
 const teamData = [
-  ["Nour", "Studying: Interactive Media Design", "On the netflix queue: The Umbrella Academy, The Walking Dead", "Fav Food: Fettucine Alfredo", "Loves: Starbucks Cold Brew and her cat Peaches"],
-
-  [
-    
-  ]
+  ['images/nourHeadshot.jpg','Nour Al-Osman', 'Designer','Studying: Interactive Media Design,<br> On the netflix queue: The Umbrella Academy, The Walking Dead,<br> Fav Food: Fettucine Alfredo,<br> Loves: Starbucks Cold Brew and her cat Peaches'],
+  ['images/glebHeadshot.jpg','Gleb Zavizenov', 'Developer', '• Best Front-End Developer in the world!<br>• Studying "Interactive Media Design"<br>• Fan of technologies and startups<br>• Favourite TV Serial - "Silicon Valley" <br>']
 ]
 
-
-// FUNCTIONS
-
-// EVENTS
