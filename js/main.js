15d0201a0fe40db04410c9bea0ad5ebc4da13ce1
nour <nour@Nours-MacBook-Pro.local>
2019-08-01 10:07:54 -0400

    nour added her info into an array

--- js/main.js
+++ js/main.js
@@ -7,9 +7,7 @@ console.log('my JS is workin!!!');
 // VARIABLES
 
 const teamData = [
-  [
-
-  ],
+  ["Nour", "Studying: Interactive Media Design", "On the netflix queue: The Umbrella Academy, The Walking Dead", "Fav Food: Fettucine Alfredo", "Loves: Starbucks Cold Brew and her cat Peaches"],
 
   [
     
